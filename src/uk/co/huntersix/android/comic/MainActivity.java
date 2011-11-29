c2854382086aaa416ad8e77f4cc2aa36d2722605
arcotc <arcotc@yahoo.co.uk>
2011-11-29 21:13:46 +0000

    Refactor to use fragments

--- src/uk/co/huntersix/android/comic/MainActivity.java
+++ src/uk/co/huntersix/android/comic/MainActivity.java
@@ -1,11 +1,11 @@
 package uk.co.huntersix.android.comic;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class SummaryDetailsActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.summary_details);
+	    setContentView(R.layout.main);
 	}
 }
---
+++ src/uk/co/huntersix/android/comic/SummaryDetailsFragment.java
@@ -0,0 +1,14 @@
+package uk.co.huntersix.android.comic;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SummaryDetailsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.summary_details, container, false);
+    }
+}
