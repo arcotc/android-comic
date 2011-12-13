82d9533d7e4cc40fc988dfab3e93b0e104f64009
arcotc <arcotc@yahoo.co.uk>
2011-12-13 20:13:27 +0000

    Added comic pages with page curl

--- src/uk/co/huntersix/android/comic/SummaryDetailsFragment.java
+++ src/uk/co/huntersix/android/comic/SummaryDetailsFragment.java
@@ -1,5 +1,11 @@
 package uk.co.huntersix.android.comic;
 
+import uk.co.huntersix.android.comic.curl.CurlView;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,8 +13,82 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SummaryDetailsFragment extends Fragment {
+	private CurlView view;
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.summary_details, container, false);
+    	view = (CurlView)inflater.inflate(R.layout.curl, container, false);
+    	
+		int index = 0;
+    	view.setBitmapProvider(new BitmapProvider());
+    	view.setSizeChangedObserver(new SizeChangedObserver());
+    	view.setCurrentIndex(index);
+    	view.setBackgroundColor(0xFF202830);
+    	
+        return view;
     }
+
+	private class BitmapProvider implements CurlView.BitmapProvider {
+		private int[] mBitmapIds = { R.drawable.book1_125x167, R.drawable.book2_125x167, R.drawable.book3_125x167 };
+
+		@Override
+		public Bitmap getBitmap(int width, int height, int index) {
+			Bitmap b = Bitmap.createBitmap(width, height,
+					Bitmap.Config.ARGB_8888);
+			b.eraseColor(0xFFFFFFFF);
+			Canvas c = new Canvas(b);
+			Drawable d = getResources().getDrawable(mBitmapIds[index]);
+
+			int margin = 7;
+			int border = 3;
+			Rect r = new Rect(margin, margin, width - margin, height - margin);
+
+			int imageWidth = r.width() - (border * 2);
+			int imageHeight = imageWidth * d.getIntrinsicHeight()
+					/ d.getIntrinsicWidth();
+			if (imageHeight > r.height() - (border * 2)) {
+				imageHeight = r.height() - (border * 2);
+				imageWidth = imageHeight * d.getIntrinsicWidth()
+						/ d.getIntrinsicHeight();
+			}
+
+			r.left += ((r.width() - imageWidth) / 2) - border;
+			r.right = r.left + imageWidth + border + border;
+			r.top += ((r.height() - imageHeight) / 2) - border;
+			r.bottom = r.top + imageHeight + border + border;
+
+			Paint p = new Paint();
+			p.setColor(0xFFC0C0C0);
+			c.drawRect(r, p);
+			r.left += border;
+			r.right -= border;
+			r.top += border;
+			r.bottom -= border;
+
+			d.setBounds(r);
+			d.draw(c);
+			return b;
+		}
+
+		@Override
+		public int getBitmapCount() {
+			return mBitmapIds.length;
+		}
+	}
+
+	/**
+	 * CurlView size changed observer.
+	 */
+	private class SizeChangedObserver implements CurlView.SizeChangedObserver {
+		@Override
+		public void onSizeChanged(int w, int h) {
+			if (w > h) {
+				view.setViewMode(CurlView.SHOW_TWO_PAGES);
+				view.setMargins(.1f, .05f, .1f, .05f);
+			} else {
+				view.setViewMode(CurlView.SHOW_ONE_PAGE);
+				view.setMargins(.1f, .1f, .1f, .1f);
+			}
+		}
+	}
 }
---
+++ src/uk/co/huntersix/android/comic/curl/CurlActivity.java
@@ -0,0 +1,142 @@
+/*
+   Copyright 2011 Harri Smått
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package uk.co.huntersix.android.comic.curl;
+
+import uk.co.huntersix.android.comic.R;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+/**
+ * Simple Activity for curl testing.
+ * 
+ * @author harism
+ */
+public class CurlActivity extends Activity {
+
+	private CurlView mCurlView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.curl);
+
+		int index = 0;
+		if (getLastNonConfigurationInstance() != null) {
+			index = (Integer) getLastNonConfigurationInstance();
+		}
+		mCurlView = (CurlView) findViewById(R.id.curl);
+		mCurlView.setBitmapProvider(new BitmapProvider());
+		mCurlView.setSizeChangedObserver(new SizeChangedObserver());
+		mCurlView.setCurrentIndex(index);
+		mCurlView.setBackgroundColor(0xFF202830);
+
+		// This is something somewhat experimental. Before uncommenting next
+		// line, please see method comments in CurlView.
+		// mCurlView.setEnableTouchPressure(true);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		mCurlView.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		mCurlView.onResume();
+	}
+
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		return mCurlView.getCurrentIndex();
+	}
+
+	/**
+	 * Bitmap provider.
+	 */
+	private class BitmapProvider implements CurlView.BitmapProvider {
+
+		private int[] mBitmapIds = { R.drawable.book1_125x167, R.drawable.book2_125x167, R.drawable.book3_125x167 };
+
+		@Override
+		public Bitmap getBitmap(int width, int height, int index) {
+			Bitmap b = Bitmap.createBitmap(width, height,
+					Bitmap.Config.ARGB_8888);
+			b.eraseColor(0xFFFFFFFF);
+			Canvas c = new Canvas(b);
+			Drawable d = getResources().getDrawable(mBitmapIds[index]);
+
+			int margin = 7;
+			int border = 3;
+			Rect r = new Rect(margin, margin, width - margin, height - margin);
+
+			int imageWidth = r.width() - (border * 2);
+			int imageHeight = imageWidth * d.getIntrinsicHeight()
+					/ d.getIntrinsicWidth();
+			if (imageHeight > r.height() - (border * 2)) {
+				imageHeight = r.height() - (border * 2);
+				imageWidth = imageHeight * d.getIntrinsicWidth()
+						/ d.getIntrinsicHeight();
+			}
+
+			r.left += ((r.width() - imageWidth) / 2) - border;
+			r.right = r.left + imageWidth + border + border;
+			r.top += ((r.height() - imageHeight) / 2) - border;
+			r.bottom = r.top + imageHeight + border + border;
+
+			Paint p = new Paint();
+			p.setColor(0xFFC0C0C0);
+			c.drawRect(r, p);
+			r.left += border;
+			r.right -= border;
+			r.top += border;
+			r.bottom -= border;
+
+			d.setBounds(r);
+			d.draw(c);
+			return b;
+		}
+
+		@Override
+		public int getBitmapCount() {
+			return mBitmapIds.length;
+		}
+	}
+
+	/**
+	 * CurlView size changed observer.
+	 */
+	private class SizeChangedObserver implements CurlView.SizeChangedObserver {
+		@Override
+		public void onSizeChanged(int w, int h) {
+			if (w > h) {
+				mCurlView.setViewMode(CurlView.SHOW_TWO_PAGES);
+				mCurlView.setMargins(.1f, .05f, .1f, .05f);
+			} else {
+				mCurlView.setViewMode(CurlView.SHOW_ONE_PAGE);
+				mCurlView.setMargins(.1f, .1f, .1f, .1f);
+			}
+		}
+	}
+
+}
